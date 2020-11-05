d215247e96cac509c04a9e08201ca17a7d9587cf
joao reis <1160600@isep.ipp.pt>
2020-11-05 11:59:07 +0000

    deck format fixes in sueca and bisca

--- src/bisca.py
+++ src/bisca.py
@@ -6,4 +6,4 @@ from cards.card_enums import DeckFormat
 class Bisca(CardGame):
     def __init__(self):
         super().__init__(cards_per_player=7, card_deck=CardDeck(
-            deck_format=DeckFormat.QUARENTA))
+            deck_format=DeckFormat.FORTY))
--- src/sueca.py
+++ src/sueca.py
@@ -7,4 +7,4 @@ class Sueca(CardGame):
 
     def __init__(self):
         super().__init__(cards_per_player=7, card_deck=CardDeck(
-            deck_format=DeckFormat.QUARENTA))
+            deck_format=DeckFormat.FORTY))
