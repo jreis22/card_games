c677699489fc95ef040e649bc5f61cf265e20741
joao reis <1160600@isep.ipp.pt>
2020-11-12 17:09:07 +0000

    parameter set methods

--- src/player.py
+++ src/player.py
@@ -7,11 +7,29 @@ from cards.card_enums import Rank, Suit
 
 
 class CardPlayer:
-    def __init__(self, card_hand: CardHand = CardHand(), player_state: PlayerStateEnum = PlayerStateEnum.PLAYING, points: int = 0):
+    def __init__(self, card_hand: CardHand = None, player_state: PlayerStateEnum = PlayerStateEnum.PLAYING, points: int = 0):
         super().__init__()
-        self.card_hand = card_hand
+        self.set_card_hand(card_hand)
         self.player_state = player_state
         self.points = points
+
+    def set_card_hand(self, new_card_hand: CardHand):
+        if new_card_hand is None:
+            self.card_hand = CardHand()
+        else:
+            self.card_hand = new_card_hand
+
+    def set_card_hand_with_list(self, card_list: [PlayingCard]):
+        if card_list is None:
+            self.card_hand = CardHand()
+        else:
+            self.card_hand = CardHand(card_list=card_list)
+
+    def show_hand(self) -> [PlayingCard]:
+        return self.card_hand.show_cards()
+
+    def number_of_cards_left(self):
+        return self.card_hand.list_size()
 
     def deal_cards(self, card_hand: [PlayingCard]):
         self.card_hand.add_cards(card_hand)
@@ -44,6 +62,9 @@ class CardPlayer:
 
     def is_playing(self) -> bool:
         return self.player_state == PlayerStateEnum.PLAYING
+
+    def set_state_playing(self):
+        self.player_state = PlayerStateEnum.PLAYING
 
     def pass_turn(self):
         self.player_state = PlayerStateEnum.PASS
